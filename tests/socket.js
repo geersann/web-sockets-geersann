c1f4cc84f353196f4f94e6b6f3c3cc779a4f21e9
mplesha <mykhaylo.plesha@gmail.com>
2024-08-19 23:22:24 +0300

    Update

--- tests/socket.js
+++ tests/socket.js
@@ -1,59 +1,27 @@
-const { expect } = require('@jest/globals');
-const WebSocket = require('ws');
-const wss = require('../server/index');
+class WebSocket {
+  constructor(url) {    
+    // `onmessage` should be overwritten by the code using the socket.
+    this._onmessage = () => { };
+    this.onopen = () => { };    
+  }
 
-describe('index.html', () => {
-  test(`WebSocket Server uses its \'clients\' property`, async () => {
+  set onmessage(handler) {
+    this._onmessage = handler
+    WebSocket.instance = this;
+  }
 
-    const ws = new WebSocket("ws://localhost:8082");
-    await new Promise(resolve => ws.onopen = resolve);    
-    
-    let clientsCount = wss.clients.size;
-    ws.close();
+  static instance = {};
 
-    expect(clientsCount).toEqual(1);
-  });  
+  addEventListener(event, cb) { 
+    WebSocket.instance = this;     
+      if(event === "message") {
+          this.onmessage = cb;
+      }
+  }
 
-  test("WebSocket Server is sending back received data", async () => {
+  send(data) {    
+    this._onmessage({data: data});
+  }
+};
 
-    const ws = new WebSocket("ws://localhost:8082");
-    await new Promise(resolve => ws.onopen = resolve);
-
-    let messageToReceive = new Promise(resolve => ws.onmessage = message => {
-      resolve(message.data);         
-    })
-    ws.send('This is test data');
-    let receivedMessage = await messageToReceive;
-    ws.close();
-
-    expect(receivedMessage.endsWith('This is test data')).toBeTruthy();
-  });
-
-  test("WebSocket Server is sending received data to all its clients", async () => {
-
-    const ws1 = new WebSocket("ws://localhost:8082");
-    const ws2 = new WebSocket("ws://localhost:8082");
-    await new Promise(resolve => ws1.onopen = resolve);
-    await new Promise(resolve => ws2.onopen = resolve);
-
-    let messageToReceive1 = new Promise(resolve => ws1.onmessage = message => {
-      resolve(message.data);         
-    })
-    let messageToReceive2 = new Promise(resolve => ws2.onmessage = message => {
-      resolve(message.data);         
-    })
-    ws1.send('This is test data');
-    let receivedMessage1 = await messageToReceive1;
-    let receivedMessage2 = await messageToReceive2;
-
-    ws1.close();
-    ws2.close();
-    
-    expect(receivedMessage1).toEqual(receivedMessage2);
-  });
-
-  afterAll(() => {  
-    wss.close();    
-  })
-
-})
+module.exports = WebSocket;
